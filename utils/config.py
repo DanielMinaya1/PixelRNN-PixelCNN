f93ce9d83960cb5e246f99b2408719f6b0b9bf03
DanielMinaya1 <dminaya@dim.uchile.cl>
2023-12-17 17:41:09 -0300

    update batch size

--- utils/config.py
+++ utils/config.py
@@ -6,5 +6,5 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 lr = 0.001
 criterion = lambda x,y: F.binary_cross_entropy(x,y)
 
-batch_size_train = 128
+batch_size_train = 16
 batch_size_test = 128
